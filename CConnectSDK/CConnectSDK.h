9131f76c4f2cd63315a73b637f026acbd19882bf
Quang Hà <haphanquang@gmail.com>
2020-05-17 20:30:11 +0700

    update core

---
+++ CConnectSDK/CConnectSDK.h
@@ -0,0 +1,19 @@
+//
+//  CConnectSDK.h
+//  CConnectSDK
+//
+//  Created by QH on 5/7/20.
+//  Copyright © 2020 LG Electronics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CConnectSDK.
+FOUNDATION_EXPORT double CConnectSDKVersionNumber;
+
+//! Project version string for CConnectSDK.
+FOUNDATION_EXPORT const unsigned char CConnectSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CConnectSDK/PublicHeader.h>
+
+
